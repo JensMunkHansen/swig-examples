fa380a1915735ade2388d805fd113bf6ff29a9b5
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2021-06-23 17:40:41 +0200

    java working in linux

--- java/uint8_vector/build.sh
+++ java/uint8_vector/build.sh
@@ -1,4 +1,5 @@
-swig3.0 -java -Wall test.i
+#!/bin/sh
+swig -java -Wall test.i
 gcc -Wall -Wextra -o libtest.so -I/usr/lib/jvm/default-java/include/ -I/usr/lib/jvm/default-java/include/linux test_wrap.c -shared 
 javac *.java  -Xlint:deprecation 
 LD_LIBRARY_PATH=. java run
