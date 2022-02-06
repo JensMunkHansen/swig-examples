d43156bd8922e4ab0e83e6e707bfbb69eaf01a28
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2022-02-07 00:17:17 +0100

    added all language test

---
+++ languages/test.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+use example;
+print $example::My_variable,"\n";
+print example::fact(5),"\n";
+print example::get_time(),"\n";
---
+++ languages/test.tcl
@@ -0,0 +1,6 @@
+#!/usr/bin/tclsh
+load ./example_tcl.so example
+puts $My_variable
+fact 5
+my_mod 7 3
+get_time
